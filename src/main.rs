07a7fb79ea144ca28c2621faefe77f5c82e11f60
wizard-lgtm <rippiten23@gmail.com>
2025-04-24 14:52:32 +0300

    implement main function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-mod types;
+// src/main.rs
+use hhcc::lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let mut lexer = Lexer::new("U32 main() { return 0; }".to_string());
+    let tokens = lexer.tokenize().unwrap();
+
+    for token in tokens {
+        println!("{:?}: {}", token.token_type, token.value);
+    }
 }
