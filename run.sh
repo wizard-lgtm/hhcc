810fbc3e504ee769b69227f91ed19974f29e280f
wizard-lgtm <rippiten23@gmail.com>
2025-05-05 01:28:23 +0300

    add test2.HC with Test class and main function; update run.sh to use test2.HC

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python3 src/compiler.py src/code/test.HC -S -emit-llvm -o output.ll
+python3 src/compiler.py src/code/test2.HC -S -emit-llvm -o output.ll
 llvm-as output.ll 
 clang output.bc -o output
 ./output
