17299914cb4c7780664da8f01c9c921bb91f099d
wizard-lgtm <rippiten23@gmail.com>
2025-05-11 14:48:17 +0300

    Update run.sh to compile test4.HC and remove obsolete test2.HC references

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-python3 src/compiler.py src/code/test2.HC -S -emit-llvm -o output.ll
-llvm-as output.ll 
-clang output.bc -o output
+python src/compiler.py src/code/test4.HC -S -emit-llvm -o output
 ./output
 echo $?
